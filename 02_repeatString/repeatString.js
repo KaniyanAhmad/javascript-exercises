62ee316b12eb93191144c9a1b95ac6c6db040092
KaniyanAhmad <kaniyanahmad1@gmail.com>
2023-08-09 08:27:48 -0700

    02 Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str,n) {
+        let temp = "";
+    if(n<=-1){
+        temp = "ERROR";
+    } else{
+        for(i=n;i>0;i--) {
+            temp+=str;
+        }
+    }
+    return temp;
 };
 
 // Do not edit below this line
